99607a393ac0792196d41060d49eb321801913cf
jon-terschan <j.terschanski@posteo.net>
2026-02-12 17:17:14 +0200

    updated local raster write to be bit more efficient and use vrt

--- STATIC_VAR/chm_postprocessing.R
+++ STATIC_VAR/chm_postprocessing.R
@@ -5,25 +5,43 @@
 # careful: NO CRS checking, so unified CRS and resolution/alignment is assumed for the input.
 
 library(terra)
+terraOptions(memfrac = 0.8)
 
-in_file  <- "C:/Users/terschan/Downloads/building_metrics/chmfill/merged_CHM_05m_Hel.tif"
-out_file <- "C:/Users/terschan/Downloads/building_metrics/chmfill/merged_CHM_05m_Hel_filled.tif"
+in_file  <- "//ad.helsinki.fi/home/t/terschan/Desktop/paper1/scripts/DATA/chm_full/merged_CHM_05m_Hel.tif"
+out_file <- "//ad.helsinki.fi/home/t/terschan/Desktop/paper1/scripts/DATA/chm_full/merged_CHM_05m_Hel_filled.tif"
 
 chm <- rast(in_file)
 
-# first pass (3x3)
-na <- is.na(chm)
-chm_filled <- chm
-chm_filled[na] <- focal(chm, w = 3, fun = max, na.rm = TRUE)[na]
-
-# second pass (5x5)
-na <- is.na(chm_filled)
-chm_filled[na] <- focal(chm_filled, w = 5, fun = max, na.rm = TRUE)[na]
-
-writeRaster(
-  chm_filled,
-  out_file,
+# first pass
+chm_filled1 <- focal(
+  chm,
+  w = 3,
+  fun = max,
+  na.rm = TRUE,
+  filename = tempfile(fileext = ".tif"),
+  overwrite = TRUE
+)
+# replace only NA
+chm_filled1 <- ifel(
+  is.na(chm),
+  chm_filled1,
+  chm
+)
+# second pass
+chm_filled2 <- focal(
+  chm_filled1,
+  w = 5,
+  fun = max,
+  na.rm = TRUE,
+  filename = out_file,
   overwrite = TRUE,
-  datatype = "FLT4S",
-  gdal = c("TILED=YES", "COMPRESS=LZW", "PREDICTOR=2", "BIGTIFF=YES")
+  wopt = list(
+    datatype = "FLT4S",
+    gdal = c(
+      "TILED=YES",
+      "COMPRESS=ZSTD",
+      "PREDICTOR=2",
+      "BIGTIFF=YES"
+    )
+  )
 )
--- STATIC_VAR/raster_merge.R
+++ STATIC_VAR/raster_merge.R
@@ -7,19 +7,41 @@
 
 # --- merge alternative ---
 library(terra) 
+terraOptions(memfrac = 0.8) # we aint have all day boy
 
 # ---- paths ----
-in_dir   <- "C:/Users/terschan/Downloads/building_metrics/chmfill/"
+in_dir   <- "//ad.helsinki.fi/home/t/terschan/Desktop/paper1/scripts/DATA/chmfill"
+out_dir <- "//ad.helsinki.fi/home/t/terschan/Desktop/paper1/scripts/DATA/chm_full"
 tag <- "CHM_05m_HEL"   # tag for whatever is the input/output name
 
-out_file <- file.path(in_dir, paste0("merged_", tag, ".tif"))
-terraOptions(memfrac = 0.8) # set terra RAM limit aggressively for faster processing
+out_file <- file.path(out_dir, paste0("merged_", tag, ".tif"))
+out_file
 
 # ---- list & merge ----
 raster_files <- list.files(in_dir, pattern="\\.tif$", full.names=TRUE)
-merged_raster <- terra::merge(raster_files)
-
 merged_raster <- do.call(terra::merge, lapply(raster_files, terra::rast))
+merged_raster <- do.call(
+  terra::merge,
+  c(lapply(raster_files, terra::rast), list(filename = out_file))
+)
+
+merged_raster <- do.call(
+  terra::merge,
+  c(
+    lapply(raster_files, terra::rast),
+    list(
+      filename = out_file,
+      wopt = list(
+        gdal = c(
+          "TILED=YES",
+          "COMPRESS=LZW",
+          "PREDICTOR=2",
+          "BIGTIFF=YES"
+        )
+      )
+    )
+  )
+)
 
 # ---- write output ----
 writeRaster(
@@ -34,3 +56,25 @@ writeRaster(
   )
 )
 
+#---alternative using vrt-----
+raster_files <- list.files(in_dir, pattern="\\.tif$", full.names=TRUE)
+
+vrt_file <- file.path(out_dir, paste0(tag, ".vrt"))
+
+vrt <- vrt(
+  raster_files,
+  filename = vrt_file,
+  overwrite = TRUE
+)
+
+writeRaster(
+  rast(vrt_file),
+  out_file,
+  overwrite = TRUE,
+  gdal = c(
+    "TILED=YES",
+    "COMPRESS=LZW",
+    "PREDICTOR=2",
+    "BIGTIFF=YES"
+  )
+)
